a8cfbbfcbff2159e805b68ab7a01fdf8e5a6f26c
PsSave <pedrosviki1265@gmail.com>
2025-04-16 13:36:48 -0300

    feat: adjust margin in IntroSection for improved layout on small screens

--- src/components/sections/intro-section.tsx
+++ src/components/sections/intro-section.tsx
@@ -26,7 +26,7 @@ export function IntroSection() {
       </section>
 
       <section className="w-full h-full justify-end items-end col-start-3 col-end-10 flex flex-col gap-20 max-sm:col-start-2 max-sm:col-end-5">
-        <div className="flex flex-col items-center mt-30 max-sm:mt-5 max-sm:mb-0">
+        <div className="flex flex-col items-center mt-30 max-sm:mt-10 max-sm:mb-0">
           <MaskText
             phrases={[
               "The rules of advertising were written before streaming, before platforms, before AI.",
